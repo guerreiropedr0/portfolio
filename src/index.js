ea9f780474aa55ebe4009cb5a04cc554ef2899e7
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2022-06-20 10:51:26 +0100

    Add navbar interaction
    Click the navbar opens the links

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import './styles/index.css';
+
+const OPEN_MENU = document.getElementById('open-menu');
+const MENU = document.getElementById('menu');
+
+OPEN_MENU.addEventListener(('click'), () => {
+  MENU.style.display = 'block';
+});
