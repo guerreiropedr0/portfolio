da850a94253847dc508eca0d21b5076ba829d042
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2022-06-20 12:04:32 +0100

    Add animation to the navbar

--- src/index.js
+++ src/index.js
@@ -1,18 +1,14 @@
 import './styles/index.css';
 
 const MENU = document.getElementById('menu');
-const MENU_INFO = document.getElementById('nav-info');
 const OPEN_MENU = document.getElementById('open-menu');
-const CLOSE_MENU = document.getElementById('close-menu');
 
-const menuDisplay = (btn) => {
-  btn.addEventListener(('click'), () => {
-    MENU.classList.toggle('hidden');
-    MENU_INFO.classList.toggle('hidden');
-  });
-};
-
-window.onload = () => {
-  menuDisplay(OPEN_MENU);
-  menuDisplay(CLOSE_MENU);
-};
+OPEN_MENU.addEventListener(('click'), () => {
+  if (MENU.style.height === '175px') {
+    MENU.style.height = '0';
+  } else {
+    MENU.style.height = '175px';
+  }
+  OPEN_MENU.classList.toggle('bi-list');
+  OPEN_MENU.classList.toggle('bi-x');
+});
