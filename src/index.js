a4a1129d850ed03db3a698e7a4f78fcbe9b1c7c5
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2022-06-20 10:59:03 +0100

    Add navbar close button interaction

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import './styles/index.css';
 
-const OPEN_MENU = document.getElementById('open-menu');
 const MENU = document.getElementById('menu');
+const OPEN_MENU = document.getElementById('open-menu');
+const CLOSE_MENU = document.getElementById('close-menu');
 
 OPEN_MENU.addEventListener(('click'), () => {
-  MENU.style.display = 'block';
+  MENU.classList.toggle('hidden');
+  OPEN_MENU.style.display = 'none';
+});
+
+CLOSE_MENU.addEventListener(('click'), () => {
+  MENU.classList.toggle('hidden');
+  OPEN_MENU.style.display = 'block';
 });
