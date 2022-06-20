c2eb50bea8881e7d816801178cf717081da2d9c4
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2022-06-20 14:47:25 +0100

    Add logo

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import './styles/index.css';
+import logo from './assets/logo.svg';
 
 const MENU = document.getElementById('menu');
 const OPEN_MENU = document.getElementById('open-menu');
+document.getElementById('logo').src = logo;
 
 OPEN_MENU.addEventListener(('click'), () => {
-  if (MENU.style.height === '175px') {
-    MENU.style.height = '0';
+  if (MENU.style.height === '184px') {
+    MENU.style.height = 0;
+    MENU.style.marginTop = 0;
   } else {
-    MENU.style.height = '175px';
+    MENU.style.marginTop = '1rem';
+    MENU.style.height = '184px';
   }
   OPEN_MENU.classList.toggle('bi-list');
   OPEN_MENU.classList.toggle('bi-x');
