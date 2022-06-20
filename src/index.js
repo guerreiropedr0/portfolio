315f2df2259af2e0c0f492f981c2b283686fe077
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2022-06-20 11:27:51 +0100

    Add logo to navbar

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import './styles/index.css';
 
 const MENU = document.getElementById('menu');
+const MENU_INFO = document.getElementById('nav-info');
 const OPEN_MENU = document.getElementById('open-menu');
 const CLOSE_MENU = document.getElementById('close-menu');
 
 const menuDisplay = (btn) => {
   btn.addEventListener(('click'), () => {
     MENU.classList.toggle('hidden');
-    OPEN_MENU.classList.toggle('hidden');
+    MENU_INFO.classList.toggle('hidden');
   });
 };
 
